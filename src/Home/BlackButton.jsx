adb58e3eb30102dcd37ecb8a3430d5785e0f2fe6
Petar Vujović <petarvujovic98@gmail.com>
2023-06-30 10:35:51 +0200

    fix: Use correct button hover effects

--- src/Home/BlackButton.jsx
+++ src/Home/BlackButton.jsx
@@ -6,7 +6,6 @@ const Anchor = styled.a`
   padding: 0.625em 1.25em;
   min-width: 10.625em;
   gap: 0.5em;
-  border: none;
   border-radius: 50px;
   background: #161615;
   color: #fff;
@@ -16,6 +15,17 @@ const Anchor = styled.a`
   line-height: 1em;
   text-align: center;
   white-space: nowrap;
+  border: 1px solid #161615;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  &:hover,
+  &:focus,
+  &:active {
+    background: #fff;
+    color: #161615;
+    text-decoration: none;
+  }
 
   &:disabled {
     background: #e5e5e5;
