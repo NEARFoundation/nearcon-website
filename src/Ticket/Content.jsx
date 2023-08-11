02c06b4d038525a2ce32f63785096f156118bf87
Zahidul Islam <zahid@cryptosynk.com>
2023-08-11 01:23:15 -0700

    ticket validation bugfix

--- src/Ticket/Content.jsx
+++ src/Ticket/Content.jsx
@@ -31,13 +31,47 @@ const Detail = styled.div`
   }
 `;
 
+const {
+  props: { contractId, secretKey },
+} = props;
+
+State.init({
+  isValidTicket: false,
+});
+
+const checkTicketValidity = (secretKey) => {
+  asyncFetch(
+    `https://kb73xf6bzk.execute-api.us-east-1.amazonaws.com/production/api/v1/keypom/${secretKey}`
+  ).then((response) => {
+    if (response.ok) {
+      const keyInfo = Near.view(contractId, "get_key_information", {
+        key: response.body.publicKey,
+      });
+
+      if (keyInfo) {
+        State.update({ isValidTicket: true });
+      }
+    }
+  });
+};
+
+if (contractId && secretKey) {
+  checkTicketValidity(secretKey);
+}
+
 return (
   <Container>
-    <Explainer>
-      <Widget src={`${ownerId}/widget/Ticket.QRCode`} props={props} />
-    </Explainer>
-    <Detail>
-      <Widget src={`${ownerId}/widget/Ticket.Detail`} props={props} />
-    </Detail>
+    {state.isValidTicket ? (
+      <>
+        <Explainer>
+          <Widget src={`${ownerId}/widget/Ticket.QRCode`} props={props} />
+        </Explainer>
+        <Detail>
+          <Widget src={`${ownerId}/widget/Ticket.Detail`} props={props} />
+        </Detail>
+      </>
+    ) : (
+      <h2>Invalid Ticket.</h2>
+    )}
   </Container>
 );
